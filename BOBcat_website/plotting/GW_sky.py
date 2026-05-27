5744facfd33df3c77d0214b7b2f8763474760b99
ThreeBoots <okjordy@icloud.com>
2026-05-27 11:21:50 -0400

    Fixing a unit issue in the strain map

--- BOBcat_website/plotting/GW_sky.py
+++ BOBcat_website/plotting/GW_sky.py
@@ -25,12 +25,14 @@ coords_rad['theta'] = np.radians(coords['dec_deg']) + np.pi/2 # Needs to be posi
 coords_rad['name'] = coords['name']
 
 # Calculate the strain for each candidate where it is calculable
+strain_calc_params['mc'] = 10**strain_calc_params['mc']
 strain_calc_params['f_grav'] = 2*strain_calc_params['orb_freq']
 strain_calc_params['Dl_Mpc'] = np.empty(len(strain_calc_params['redshift']))
-strain_calc_params.dropna(inplace=True)
 strain_calc_params.reset_index(drop=True, inplace=True)
 for i in range(len(strain_calc_params['redshift'])):
     strain_calc_params.loc[i,'Dl_Mpc'] = calc.cosmo_calc(strain_calc_params.loc[i,'redshift'])[0]
+strain_calc_params.dropna(inplace=True)
+strain_calc_params.reset_index(drop=True, inplace=True)
 strains = pd.DataFrame(strain_calc_params['name'])
 h = pd.DataFrame()
 h['h'] = pd.DataFrame([calc.strain_calc(strain_calc_params.loc[i,'mc'],strain_calc_params.loc[i,'Dl_Mpc'],strain_calc_params.loc[i,'f_grav']) for i in range(len(strain_calc_params))])
@@ -42,13 +44,13 @@ coords_rad_strains = coords_rad.merge(strains, left_on='name', right_on='name')
 # Generate the sky map
 NSIDE = 128
 NPIX = hp.nside2npix(NSIDE)
-m = np.ones(NPIX)*10**-35
+m = np.ones(NPIX)*min(strains['h'])/10
 candidate_pixels = hp.ang2pix(NSIDE, coords_rad['theta'], coords_rad['phi'])
 for pixel in candidate_pixels:
     m[pixel] = np.nan
 for obj in coords_rad_strains.iterrows():
     pixel = hp.ang2pix(NSIDE, obj[1]['theta'], obj[1]['phi'])
     m[pixel] = obj[1]['h']
-hp.mollview(m, title="Sky Map of Candidates", unit='strain', badcolor='white', flip='astro', coord='C', norm='log', rot=(0,0,180))
+hp.mollview(m, title="Sky Map of Candidates", unit='strain', badcolor='red', flip='astro', coord='C', norm='log', rot=(0,0,180))
 hp.graticule(dpar=30,dmer=30,coord='C')
 plt.show()
